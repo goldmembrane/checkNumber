fbbf69ca3c2b74ed6f1ada8015d1152f56ae44c2
goldmembrane <byenghyunhwang@Byengui-MacBook-Pro.local>
2020-08-26 10:31:09 +0900

    create description modal and insert gif

--- client/src/game/Nav.js
+++ client/src/game/Nav.js
@@ -1,10 +1,27 @@
 import React from "react";
 import Calculater from "./Calculater";
 import "./css/Nav.css";
+import Popup from "reactjs-popup";
+import Description from "./Description";
 
 function setRandomNumber(props) {
   return (
     <div className="setRandomNumber">
+      <Popup
+        trigger={<button className="show-description">설명 보기</button>}
+        position="right center"
+        modal={true}
+        contentStyle={{ maxWidth: "1024px", width: "90%", height: "90%" }}
+      >
+        {(close) => (
+          <div>
+            <div className="close-desc" onClick={close}>
+              X
+            </div>
+            <Description close={close} />
+          </div>
+        )}
+      </Popup>
       <div className="printNumber">{props.number}</div>
       {!props.number ? (
         <button className="settingBtn" onClick={() => props.set(props.number)}>
