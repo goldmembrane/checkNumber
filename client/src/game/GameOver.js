70f753d23378c0946677b375209f6305591679d8
goldmembrane <byenghyunhwang@Byengui-MacBook-Pro.local>
2020-08-28 14:37:52 +0900

    create input name box at gameover page

--- client/src/game/GameOver.js
+++ client/src/game/GameOver.js
@@ -1,41 +1,67 @@
 import React from "react";
 import "./css/GameOver.scss";
 
-const GameOver = (props) => {
-  return props.defeat > 2 ? (
-    <div className="game-over-box">
-      <div className="game-over">Game Over</div>
-      <div className="score">your score: {props.score}</div>
-      <div className="save-box">
-        <span className="save-temporary">Save score!</span>
-        <button
-          className="save"
-          onClick={(e) => {
-            e.preventDefault();
-            fetch("http://localhost:8000/rank/post", {
-              method: "POST",
-              body: JSON.stringify({ score: props.score }),
-              credentials: "include",
-              headers: {
-                "Content-type": "application/json",
-              },
-            })
-              .then((res) => {
-                if (res.status === 201) {
-                  console.log("Save Success");
-                }
+class GameOver extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: "",
+    };
+    this.textChange = this.textChange.bind(this);
+  }
+
+  textChange(event) {
+    this.setState({ text: event.target.value });
+  }
+
+  render() {
+    return this.props.defeat > 2 ? (
+      <div className="game-over-box">
+        <div className="game-over">Game Over</div>
+        <div className="score">your score: {this.props.score}</div>
+        <div className="name-box">
+          <label className="name-label">name:</label>
+          <input
+            type="text"
+            className="write"
+            value={this.state.text}
+            onChange={this.textChange}
+          />
+        </div>
+        <div className="save-box">
+          <span className="save-temporary">Save score!</span>
+          <button
+            className="save"
+            onClick={(e) => {
+              e.preventDefault();
+              fetch("http://localhost:8000/rank/post", {
+                method: "POST",
+                body: JSON.stringify({
+                  score: this.props.score,
+                  username: this.state.text,
+                }),
+                credentials: "include",
+                headers: {
+                  "Content-type": "application/json",
+                },
               })
-              .catch((error) => {
-                console.log(error);
-              });
-            props.clear();
-          }}
-        >
-          Save score!
-        </button>
+                .then((res) => {
+                  if (res.status === 201) {
+                    console.log("Save Success");
+                  }
+                })
+                .catch((error) => {
+                  console.log(error);
+                });
+              this.props.clear();
+            }}
+          >
+            Save score!
+          </button>
+        </div>
       </div>
-    </div>
-  ) : null;
-};
+    ) : null;
+  }
+}
 
 export default GameOver;
--- client/src/game/Ranking.js
+++ client/src/game/Ranking.js
@@ -4,7 +4,7 @@ const Ranking = (props) => {
   return (
     <div className="ranking-slot">
       <div className="rank">
-        <div className="rank-username">nick{props.rank.username}</div>
+        <div className="rank-username">{props.rank.username}</div>
         <div className="rank-score">{props.rank.score}개</div>
       </div>
     </div>
