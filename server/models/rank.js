83cc3e161341090ddc30368f748a6eaf53e5c1cb
goldmembrane <byenghyunhwang@Byengui-MacBook-Pro.local>
2020-08-28 09:26:48 +0900

    create database, models and do migration

---
+++ server/models/rank.js
@@ -0,0 +1,26 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class Rank extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Rank.init(
+    {
+      username: DataTypes.STRING,
+      score: DataTypes.INTEGER,
+    },
+    {
+      sequelize,
+      modelName: "Rank",
+      timestamps: false,
+    }
+  );
+  return Rank;
+};
